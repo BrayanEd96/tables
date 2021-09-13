7797d37719eb26776134d0be071f88f49bd596a6
Brayan Velazquez <brayane96@outlook.es>
2021-09-13 02:50:51 -0500

    Primeros cambios de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
 import { Component } from '@angular/core';
+import { ColumnMode, columnsByPin } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exampleT';
+
+  UdeTrabajo = "";
+  emp = "";
+  ub = "";
+
+  rows = [
+    {
+      name: 'Claudine Neal',
+      ubicacion: 'activo',
+      gender: 'female',
+      empresa: '123123412',
+      company: 'Sealoud',
+      udt: '100'
+    },
+    {
+      name: 'Beryl Rice',
+      ubicacion: 'activo',
+      gender: 'male',
+      empresa: '987513516',
+      company: 'Velity',
+      udt: '103'
+    },
+    {
+      name: 'Beryl Rice',
+      ubicacion: 'activo',
+      gender: 'female',
+      empresa: '123123412',
+      company: 'Velity',
+      udt: '103'
+    }
+  ];
+
+  columns = [{ name: 'Name' },{ name: 'ubicacion' }, { name: 'Gender' },{ name: 'Empresa' }, { name: 'Company' }, { name: 'udt' }];
+
+  allColumns = [{ name: 'Name' },{ name: 'ubicacion' }, { name: 'Gender' },{ name: 'Empresa' }, { name: 'Company' }, { name: 'udt' }];
+
+  valores = Object.values(this.columns);
+
+
+  ColumnMode = ColumnMode;
+
+  toggle(col) {
+    const isChecked = this.isChecked(col);
+
+    if (isChecked) {
+      this.columns = this.columns.filter(c => {
+        return c.name !== col.name;
+      });
+    } else {
+      this.columns = [...this.columns, col];
+    }
+  }
+
+  isChecked(col) {
+    return (
+      this.columns.find(c => {
+        return c.name === col.name;
+      }) !== undefined
+    );
+  }
+
+  evento(){
+    console.log(this.valores);
+    
+    if(this.UdeTrabajo !== ""){
+      this.columns = this.columns.filter(c => {
+        return c.name !== "udt";
+      });
+    }else{
+      let col = { name: 'udt' };
+      this.columns = [...this.columns, col];
+      // let i = this.valores.findIndex(e => e.name == "udt");   te quedaste aqui
+      // let nw;
+      // nw = this.columns.map(x => {
+      //   r
+      // });
+    }
+    if(this.emp !== ""){
+      this.columns = this.columns.filter(c => {
+        return c.name !== "Empresa";
+      });
+    }else{
+      let col = { name: 'Empresa' };
+      this.columns = [...this.columns, col];
+    }
+  }
+
+
+  
+  // rows = [
+  //   { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+  //   { name: 'Dany', gender: 'Male', company: 'KFC' },
+  //   { name: 'Molly', gender: 'Female', company: 'Burger King' }
+  // ];
+  // columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Company' }];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxDatatableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
